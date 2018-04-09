4907eb00c465b0c7157a2804c26f19e6a2c6507b
Michael <mikee_f1@hotmail.com>
2018-04-09 10:08:05 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Michael Fisher on 03/04/2018.
+//  Copyright © 2018 Michael Fisher. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
+
